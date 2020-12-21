e38e5c31b4b3718b19e71c1f5094a1f4ade2e115
Manfredi Mauro Marrone <manfredi.marrone@hotmail.it>
2020-12-21 17:32:22 +0100

    creato file home e linkato css

---
+++ resources/views/home.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Prima app in laravel</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css')}}">
+  </head>
+  <body>
+    <h1>hello world</h1>
+  </body>
+</html>
